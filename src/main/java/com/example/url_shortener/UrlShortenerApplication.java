a0d2f78cda99239a3b2d66353765213d1087f126
Trinabh <1by23cs275@bmsit.in>
2026-07-04 10:33:18 +0530

    feat: add frontend and CORS configuration

--- src/main/java/com/example/url_shortener/UrlShortenerApplication.java
+++ src/main/java/com/example/url_shortener/UrlShortenerApplication.java
@@ -3,7 +3,10 @@ package com.example.url_shortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableCaching
@@ -13,5 +16,15 @@ public class UrlShortenerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST");
+			}
+		};
+	}
 }
